e8d87bf52aeb992790395ce9f13362a4dbc8e179
failury <2760085283@qq.com>
2020-01-30 23:33:46 -0800

    project setup

---
+++ partB/cmake-build-debug/CMakeFiles/partB.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/partB.dir/main.cpp.o"
+  "partB.pdb"
+  "partB.exe"
+  "libpartB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/partB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
